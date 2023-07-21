b2d0d5abe3bbbec032978c2b42b251a373338936
小宮隆 <>
2023-07-21 13:25:55 +0900

    3rd commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <>
     <div className="h-screen flex justify-center items-center">
       <div className="flex flex-col">
-       <div class="flex justify-center  items-center gap-6 h-14 p-3">
+       <div className="flex justify-center  items-center gap-6 h-14 p-3">
       <h2 className="text-2xl font-bold tracking-wider">404</h2>
       <p className="border-r h-10 border-gray-900">
       </p>
